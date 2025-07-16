5f7c17c9711d811cb2fa50ee8e00799f9cff37f3
Abrar Khawar <akabrar5@gmail.com>
2025-07-17 00:33:14 +0530

    structured the frontend folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Dashboard from './pages/Dashboard'
+
+function App() {
+  return (
+      <div>
+        {/* abliHUB starts from here */}
+        <Dashboard />
+      </div>
+  )
+}
+
+export default App
